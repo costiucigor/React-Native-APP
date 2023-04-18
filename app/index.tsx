3646455dd6634d567ea51cdaecc7990b4c0ea752
Costiuc Igor <90152687+Xenilae@users.noreply.github.com>
2023-04-18 23:13:02 +0300

    added maps

--- app/index.tsx
+++ app/index.tsx
@@ -1,163 +1,100 @@
-import {useEffect, useState} from 'react';
-import {Alert} from 'react-native';
-import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
-import {defaultLocation} from '../utils/vars';
-import {getDistanceFromLatLonInKm} from '../utils/getDistance';
+import React, {useState, useEffect} from 'react';
+import {View, TouchableOpacity, Text, PermissionsAndroid} from 'react-native';
+import MapView, {Polyline} from 'react-native-maps';
 
-const useTracking = (isActive: boolean) => {
-    const [location, setLocation] = useState(defaultLocation);
-    const [history, setHistory] = useState<any>([]);
-    const [distance, setDistance] = useState<number>(0);
+const MapScreen = () => {
+    const [coordinates, setCoordinates] = useState([]);
+    const [isRecording, setIsRecording] = useState(false);
+    const [region, setRegion] = useState(null);
+    const [locationPermission, setLocationPermission] = useState('');
 
     useEffect(() => {
-        if (!isActive) {
-            return;
-        }
-        BackgroundGeolocation.configure({
-            desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
-            stationaryRadius: 50,
-            distanceFilter: 50,
-            notificationTitle: 'Background tracking',
-            notificationText: 'enabled',
-            //debug: true,
-            startOnBoot: false,
-            stopOnTerminate: true,
-            locationProvider: BackgroundGeolocation.DISTANCE_FILTER_PROVIDER, // DISTANCE_FILTER_PROVIDER for
-            interval: 10000,
-            fastestInterval: 5000,
-            activitiesInterval: 10000,
-            stopOnStillActivity: false,
-            url: 'http://192.168.81.15:3000/location',
-            httpHeaders: {
-                'X-FOO': 'bar',
-            },
-            // customize post properties
-            postTemplate: {
-                lat: '@latitude',
-                lon: '@longitude',
-                foo: 'bar', // you can also add your own properties
-            },
-        });
-
-        BackgroundGeolocation.on('location', (location) => {
-            console.log('loc', location);
-            setLocation((prev) => ({
-                ...prev,
-                latitude: location.latitude,
-                longitude: location.longitude,
-            }));
-
-            setHistory((prev) => {
-                setDistance((prevDistance) => {
-                    if (prev.length === 0) {
-                        return 0;
-                    }
-                    const latestItem = prev[prev.length - 1];
-                    return (
-                        prevDistance +
-                        getDistanceFromLatLonInKm(
-                            latestItem.latitude,
-                            latestItem.longitude,
-                            location.latitude,
-                            location.longitude,
-                        )
-                    );
-                });
-
-                return prev.concat({
-                    latitude: location.latitude,
-                    longitude: location.longitude,
-                });
-            });
-            // handle your locations here
-            // to perform long running operation on iOS
-            // you need to create background task
-            BackgroundGeolocation.startTask((taskKey) => {
-                // execute long running task
-                // eg. ajax post location
-                // IMPORTANT: task has to be ended by endTask
-                BackgroundGeolocation.endTask(taskKey);
-            });
-        });
-
-        BackgroundGeolocation.on('stationary', (stationaryLocation) => {
-            // handle stationary locations here
-        });
-
-        BackgroundGeolocation.on('error', (error) => {
-            //console.log('[ERROR] BackgroundGeolocation error:', error);
-        });
-
-        BackgroundGeolocation.on('start', () => {
-            //console.log('[INFO] BackgroundGeolocation service has been started');
-        });
-
-        BackgroundGeolocation.on('stop', () => {
-            //console.log('[INFO] BackgroundGeolocation service has been stopped');
-        });
-
-        BackgroundGeolocation.on('authorization', (status) => {
-            console.log(
-                '[INFO] BackgroundGeolocation authorization status: ' + status,
-            );
-            if (status !== BackgroundGeolocation.AUTHORIZED) {
-                // we need to set delay or otherwise alert may not be shown
-                setTimeout(
-                    () =>
-                        Alert.alert(
-                            'App requires location tracking permission',
-                            'Would you like to open app settings?',
-                            [
-                                {
-                                    text: 'Yes',
-                                    onPress: () => BackgroundGeolocation.showAppSettings(),
-                                },
-                                {
-                                    text: 'No',
-                                    onPress: () => console.log('No Pressed'),
-                                    style: 'cancel',
-                                },
-                            ],
-                        ),
-                    1000,
+        const requestLocationPermission = async () => {
+            try {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                    {
+                        title: 'Location Permission',
+                        message: 'This app needs access to your location',
+                        buttonNeutral: 'Ask Me Later',
+                        buttonNegative: 'Cancel',
+                        buttonPositive: 'OK',
+                    },
                 );
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                    setLocationPermission('Location permission granted');
+                } else {
+                    setLocationPermission('Location permission denied');
+                }
+            } catch (err) {
+                console.warn(err);
             }
-        });
-
-        BackgroundGeolocation.on('background', () => {
-            console.log('[INFO] App is in background');
-        });
-
-        BackgroundGeolocation.on('foreground', () => {
-            console.log('[INFO] App is in foreground');
-        });
-
-        BackgroundGeolocation.checkStatus((status) => {
-            console.log(
-                '[INFO] BackgroundGeolocation service is running',
-                status.isRunning,
-            );
-            console.log(
-                '[INFO] BackgroundGeolocation services enabled',
-                status.locationServicesEnabled,
-            );
-            console.log(
-                '[INFO] BackgroundGeolocation auth status: ' + status.authorization,
-            );
-
-            // you don't need to check status before start (this is just the example)
-            if (!status.isRunning) {
-                BackgroundGeolocation.start(); //triggers start on start event
-}
-        });
-
-        return () => {
-            console.log('Removing all listeners');
-            BackgroundGeolocation.removeAllListeners();
         };
-    }, [location, isActive]);
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const {latitude, longitude} = position.coords;
+                setRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                });
+            },
+            error => console.log(error),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+        )
+        let watchId;
+        if (isRecording) {
+            watchId = navigator.geolocation.watchPosition(
+                position => {
+                    const {latitude, longitude} = position.coords;
+                    setCoordinates(coords => [...coords, {latitude, longitude}]);
+                },
+                error => console.log(error),
+                {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+            );
+        };
+        requestLocationPermission();
+        return () => {
+            if (watchId) {
+                navigator.geolocation.clearWatch(watchId);
+            }
+        };
+    }, [isRecording]);
 
-    return {location, history, distance};
+    if (!region) {
+        return <Text>Loading...</Text>;
+    }
+
+    const handleStartRecording = () => {
+        setIsRecording(true);
+    };
+
+    const handleStopRecording = () => {
+        setIsRecording(false);
+    };
+
+    return (
+        <View style={{flex: 1}}>
+            <Text>{locationPermission}</Text>
+            <MapView
+                style={{flex: 1}}
+                initialRegion={region}>
+                <Polyline coordinates={coordinates} strokeColor="#000" strokeWidth={6}/>
+            </MapView>
+            <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+                {isRecording ? (
+                    <TouchableOpacity onPress={handleStopRecording}>
+                        <Text style={{fontSize: 20, color: 'red'}}>Stop</Text>
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity onPress={handleStartRecording}>
+                        <Text style={{fontSize: 20, color: 'green'}}>Start</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
+        </View>
+    );
 };
 
-export default useTracking;
+export default MapScreen;
